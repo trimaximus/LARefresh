8d7b2f42efde0d72c69a60e4f483ad9b24cafdfa
trimaximus <trimaximus9@gmail.com>
2017-08-12 20:06:41 +0800

    First blood...

---
+++ LARefresh/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LARefresh
+//
+//  Created by Trimaximus on 12/08/17.
+//  Copyright © 2017年 LiteAtom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
